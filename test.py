1c18491174a326e629f0af42233e8bbe3a64fe27
Elie Bohbot <elie.bohbot@polytechnique.edu>
2018-01-28 22:07:42 +0100

    UZ efficient price

--- test.py
+++ test.py
@@ -57,13 +57,13 @@ Both seem to work well
 
 Question : how to compute std's of estimators?
 """
-vol_1 = 0.1
-vol_2 = 0.02
-rho = 0.9
-intensity = 300
-s_0 = 100
-
-df_X, df_Y = simul.sync_black_scholes_df(intensity, rho, vol_1, vol_2, s_0, s_0, 0.1, 0.01)
+# vol_1 = 0.1
+# vol_2 = 0.02
+# rho = 0.9
+# intensity = 300
+# s_0 = 100
+#
+# df_X, df_Y = simul.sync_black_scholes_df(intensity, rho, vol_1, vol_2, s_0, s_0, 0.1, 0.01)
 
 # print(df_X.head())
 # print(df_X.tail())
@@ -78,15 +78,15 @@ df_X, df_Y = simul.sync_black_scholes_df(intensity, rho, vol_1, vol_2, s_0, s_0,
 # plt.step(df_X.index, df_X['price_tick'], 'r', linewidth=0.7)
 #
 # plt.show()
-
-M = 50
-m = []
-for i in range(M):
-    df_X = simul.sync_black_scholes_df(intensity, rho, vol_1, vol_2, s_0, s_0, 0.1, 0.01)[0]
-    m.append(uz.eta_hat(df_X))
-
-print('mean =', np.mean(m))
-print('std =', np.std(m))
+#
+# M = 50
+# m = []
+# for i in range(M):
+#     df_X = simul.sync_black_scholes_df(intensity, rho, vol_1, vol_2, s_0, s_0, 0.1, 0.01)[0]
+#     m.append(uz.eta_hat(df_X))
+#
+# print('mean =', np.mean(m))
+# print('std =', np.std(m))
 #
 # print('Exact value :', rho, "\n")
 #
@@ -103,7 +103,6 @@ Case 2:
 Asynchronous case
 """
 
-<<<<<<< HEAD
 # vol_1 = 0.1
 # vol_2 = 0.2
 # rho = 0.6
@@ -117,19 +116,29 @@ Asynchronous case
 #
 # estim_cov_hy = hy.hayashi_yoshida(df_X, df_Y)
 # print('Value of rho by Hayashi-Yoshida :', estim_cov_hy)
-=======
 vol_1 = 0.1
 vol_2 = 0.2
 rho = 0.6
-intensity_1 = 500
-intensity_2 = 450
+intensity_1 = 200
+intensity_2 = 150
 s_0 = 100
 
-df_X, df_Y = simul.black_scholes_df(intensity_1, intensity_2, rho, vol_1, vol_2, s_0, s_0, 2, 3)
-#estim_cov_hy = 0
-estim_cov_hy = hy.hayashi_yoshida(df_X, df_Y)
-estim_cov_hy_lin = hy.hayashi_yoshida_lin(df_X, df_Y)
-print('Exact value of covariance:', rho * vol_2 * vol_1, "\n")
-print('Value of rho by Hayashi-Yoshida :', estim_cov_hy)
-print('Value of rho by Hayashi-Yoshida lin :', estim_cov_hy_lin)
->>>>>>> origin/master
+# df_X, df_Y = simul.black_scholes_df(intensity_1, intensity_2, rho, vol_1, vol_2, s_0, s_0, 2, 3)
+# #estim_cov_hy = 0
+# estim_cov_hy = hy.hayashi_yoshida(df_X, df_Y)
+# estim_cov_hy_lin = hy.hayashi_yoshida_lin(df_X, df_Y)
+#
+#
+# print('Exact value of covariance:', rho * vol_2 * vol_1, "\n")
+# print('Value of rho by Hayashi-Yoshida :', estim_cov_hy)
+# print('Value of rho by Hayashi-Yoshida lin :', estim_cov_hy_lin)
+
+
+M = 30
+m = []
+for i in range(M):
+    df_X, df_Y = simul.black_scholes_df(intensity_1, intensity_2, rho, vol_1, vol_2, s_0, s_0, 2, 3)
+    m.append(hy.hayashi_yoshida_lin(df_X, df_Y))
+
+print('mean =', np.mean(m))
+print('std =', np.std(m))
--- uncertainty_zones.py
+++ uncertainty_zones.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def eta_hat(df):
+def eta_hat_df(df):
     N_total, N_a, N_c = 0, 0, 0
     N = df.shape[0]
     """Let's say we start with an upward move"""
@@ -21,3 +21,44 @@ def eta_hat(df):
                 N_c += 1
 
     return 0.5 * (N_c / N_a)
+
+"""
+Another version with numpy arrays as input
+More convenient 
+"""
+def eta_hat(time, price):
+    N_total, N_a, N_c = 0, 0, 0
+    N = time.shape[0]
+    """Let's say we start with an upward move"""
+    up = True
+    for i in np.arange(1, N):
+        if price[i] > price[i-1]:
+            if up:
+                N_c += 1
+            else:
+                N_a += 1
+                up = True
+        if price[i] < price[i-1]:
+            if up:
+                N_a += 1
+                up = False
+            else:
+                N_c += 1
+
+    return 0.5 * (N_c / N_a)
+
+
+"""
+A method to estimate the efficient price from observed prices, the tick value, and eta_hat
+"""
+def efficient_prices(observed_prices, tick, eta_hat):
+    def sign(x):
+        if x >= 0:
+            return 1
+        else:
+            return -1
+    N = observed_prices.shape[0]
+    efficient = np.zeros(N)
+    for i in np.arange(1, N):
+        efficient[i] = observed_prices[i] - tick * (0.5 - eta_hat) * sign(observed_prices[i] - observed_prices[i-1])
+    return efficient
